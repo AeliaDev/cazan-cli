294cb41103df3a88caefd78c599369fef4a9d0a7
Sellig6792 <sellig6792@gmail.com>
2023-09-17 17:47:27 +0200

    Add version option (use fmt and clippy)

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -4,10 +4,13 @@ use argh::FromArgs;
 pub use subcommands::Subcommand;
 
 #[derive(FromArgs, Debug)]
-#[argh(name = "cazan", description = "The Command Line Tool to install to build your Cazan project")]
-pub(crate) struct CLI {
+#[argh(
+    name = "cazan",
+    description = "The Command Line Tool to install to build your Cazan project"
+)]
+pub(crate) struct Cli {
     #[argh(option, short = 'v', description = "print version info")]
-    version: bool,
+    pub(crate) version: bool,
 
     #[argh(subcommand)]
     pub subcommand: Option<Subcommand>,
