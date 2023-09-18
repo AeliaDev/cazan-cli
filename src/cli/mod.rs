86542d0d8f6b2d363fdf9a0840153eab7e5e618c
Sellig6792 <sellig6792@gmail.com>
2023-09-18 21:03:33 +0200

    Add init subcommand and allow re-initialization
    This commit adds a new `init` subcommand to the CLI for creating a new Cazan project. This includes the creation of a new directory named `.cazan` in the current working directory, along with some default files. Furthermore, a `--re-init` flag has also been introduced which will allow for the re-initialization of an existing Cazan project. This means an existing `.cazan` directory and all of its contents can now be overwritten.

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,7 +1,8 @@
+mod init;
 mod subcommands;
 
 use argh::FromArgs;
-pub use subcommands::Subcommand;
+pub use subcommands::{SubCommandEnum, SubCommandTrait};
 
 #[derive(FromArgs, Debug)]
 #[argh(
@@ -9,9 +10,9 @@ pub use subcommands::Subcommand;
     description = "The Command Line Tool to install to build your Cazan project"
 )]
 pub(crate) struct Cli {
-    #[argh(option, short = 'v', description = "print version info")]
+    #[argh(switch, short = 'v', description = "print version info")]
     pub(crate) version: bool,
 
     #[argh(subcommand)]
-    pub subcommand: Option<Subcommand>,
+    pub subcommand: Option<SubCommandEnum>,
 }
