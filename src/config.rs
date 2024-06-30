1cd9437c4bee9e7ec3fe8518c7fa93afc655125e
Gilles Meunier <sellig6792@gmail.com>
2024-06-30 14:52:26 +0200

    Edit default config at init

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,22 @@
+use std::path::Path;
 use semver::Version;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
-pub struct BasicConfig<'a> {
+pub struct Config<'a> {
     pub name: &'a str,
     pub version: Version,
     pub authors: Vec<&'a str>,
-    #[serde(rename(serialize = "fileHashSalt"))]
-    pub file_hash_salt: &'a str,
+    #[serde(rename(serialize = "fileHashSalt", deserialize = "fileHashSalt"))]
+    pub file_hash_salt: Option<&'a str>,
+    #[serde(skip_serializing, rename(serialize = "useAutoplayForMultimedia", deserialize = "useAutoplayForMultimedia"))]
+    pub use_autoplay_for_multimedia: Option<bool>,
+    pub plugins: Option<Vec<PluginConfig<'a>>>
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct PluginConfig<'a> {
+    pub name: &'a str,
+    pub version: Option<Version>,
+    pub path: Option<&'a Path>
 }
