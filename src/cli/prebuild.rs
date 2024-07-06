05f9066801268a83dd0d07ddcfd504c4fc981b48
Gilles Meunier <sellig6792@gmail.com>
2024-07-06 20:31:57 +0200

    Add `--open` argument to `prebuild` command to open file explorer at the previews directory

--- src/cli/prebuild.rs
+++ src/cli/prebuild.rs
@@ -4,21 +4,23 @@
 
 use std::error::Error;
 use std::fs;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::ExitCode;
 use std::sync::{Arc, Mutex};
 
 use super::SubCommandTrait;
 use crate::config::{checksum, Config};
 use crate::terminal::SubTerminal;
-use argh::FromArgs;
+
+use cazan_common::geometry::Triangle;
 use cazan_common::rdp::rdp;
 use cazan_common::{image::ImageEdgesParser, triangulation::triangulate};
-use cazan_common::geometry::Triangle;
+
+use argh::FromArgs;
 use cprint::{ceprintln, cformat, cprintln};
 use image::GenericImageView;
-use serde_json::{json, Value};
 use plotters::prelude::*;
+use serde_json::{json, Value};
 
 const DEFAULT_EPSILON: f64 = 3.0;
 
@@ -42,9 +44,15 @@ pub struct PreBuild {
     #[argh(
         switch,
         short = 'p',
-        description = "create preview files to compare hit-boxes with the original images",
+        description = "create preview files to compare hit-boxes with the original images"
     )]
     pub preview: bool,
+
+    #[argh(
+        switch,
+        description = "open the folder to the preview files (do not use without --prebuild)"
+    )]
+    pub open: bool,
 }
 
 fn read_dir_recursive(dir: &std::path::Path) -> Vec<std::path::PathBuf> {
@@ -63,6 +71,10 @@ fn read_dir_recursive(dir: &std::path::Path) -> Vec<std::path::PathBuf> {
 
 impl SubCommandTrait for PreBuild {
     fn run(&self) -> ExitCode {
+        if self.open && !self.preview {
+            cprintln!("Warning use of `--open` without `--preview` is useless" => Yellow);
+        }
+
         let current_dir = std::env::current_dir().unwrap();
         let cazan_directory = current_dir.join(".cazan");
         if !cazan_directory.exists() {
@@ -84,9 +96,9 @@ impl SubCommandTrait for PreBuild {
             .asset_dirs
             .iter()
             .flat_map(|dir| read_dir_recursive(dir.as_ref()))
-            .collect::<Vec<std::path::PathBuf>>();
+            .collect::<Vec<PathBuf>>();
 
-        let files: Vec<std::path::PathBuf> = files
+        let files: Vec<PathBuf> = files
             .iter()
             .filter(|file| {
                 file.extension()
@@ -145,7 +157,9 @@ impl SubCommandTrait for PreBuild {
                 fs::create_dir(&c).expect("Error creating the cazan temp directory");
             }
             c
-        } else { PathBuf::new() };
+        } else {
+            PathBuf::new()
+        };
 
         let mut warnings: Vec<String> = vec![];
 
@@ -154,9 +168,11 @@ impl SubCommandTrait for PreBuild {
 
             if self.preview && file.extension() == Some("png".as_ref()) {
                 match preview(&file, &triangles, &cazan_tmp) {
-                    Ok(_) => {},
-                    Err(e) => warnings.push(format!("Warning `{}` preview couldn't have been created: {e}", file.to_str().unwrap()))
-
+                    Ok(_) => {}
+                    Err(e) => warnings.push(format!(
+                        "Warning `{}` preview couldn't have been created: {e}",
+                        file.to_str().unwrap()
+                    )),
                 }
             }
 
@@ -176,34 +192,56 @@ impl SubCommandTrait for PreBuild {
             ceprintln!("Error creating `.cazan/build` directory")
         }
 
-
         let mut writer = fs::File::create(cazan_build_directory.join("assets.json")).unwrap();
         serde_json::to_writer(&mut writer, &map).unwrap();
 
+        if self.preview && self.open {
+            open::that(cazan_tmp).expect("Couldn't open file explorer");
+        }
         ExitCode::SUCCESS
     }
 }
 
-fn preview(file: &PathBuf, triangles: &Vec<Triangle>, cazan_tmp: &PathBuf) -> Result<(), Box<dyn Error>> {
-    let image = image::open(&file)?;
-    let save_path = cazan_tmp.join(file.file_stem().unwrap().to_str().unwrap().to_owned() + "-" + &checksum(file)?[0..5] + "." + file.extension().unwrap().to_str().unwrap());
+fn preview(
+    file: &PathBuf,
+    triangles: &Vec<Triangle>,
+    cazan_tmp: &Path,
+) -> Result<(), Box<dyn Error>> {
+    let image = image::open(file)?;
+    let save_path = cazan_tmp.join(
+        file.file_stem().unwrap().to_str().unwrap().to_owned()
+            + "-"
+            + &checksum(file)?[0..5]
+            + "."
+            + file.extension().unwrap().to_str().unwrap(),
+    );
 
     let root = BitMapBackend::new(&save_path, image.dimensions()).into_drawing_area();
-    let mut chart = ChartBuilder::on(&root).build_cartesian_2d(0..image.dimensions().0, image.dimensions().1..0)?;
+    let mut chart = ChartBuilder::on(&root)
+        .build_cartesian_2d(0..image.dimensions().0, image.dimensions().1..0)?;
 
-    let rgb = image.to_rgba8().chunks(4).map(|rgba| {
-        let (r, g, b, a) = (rgba[0] as i32, rgba[1] as i32, rgba[2] as i32, rgba[3] as f64 / 255.);
+    let rgb = image
+        .to_rgba8()
+        .chunks(4)
+        .flat_map(|rgba| {
+            let (r, g, b, a) = (
+                rgba[0] as i32,
+                rgba[1] as i32,
+                rgba[2] as i32,
+                rgba[3] as f64 / 255.,
+            );
 
-        vec![((1. - a) * 255. + a * r as f64) as u8,
-             ((1. - a) * 255. + a * g as f64) as u8,
-             ((1. - a) * 255. + a * b as f64) as u8,
-        ]
-    }).flatten().collect::<Vec<u8>>();
+            vec![
+                ((1. - a) * 255. + a * r as f64) as u8,
+                ((1. - a) * 255. + a * g as f64) as u8,
+                ((1. - a) * 255. + a * b as f64) as u8,
+            ]
+        })
+        .collect::<Vec<u8>>();
 
     let elem = BitMapElement::with_owned_buffer((0, 0), image.dimensions(), rgb).unwrap();
 
     root.draw(&elem)?;
-
 
     let line_style = ShapeStyle {
         color: RED.mix(0.6),
@@ -212,16 +250,22 @@ fn preview(file: &PathBuf, triangles: &Vec<Triangle>, cazan_tmp: &PathBuf) -> Re
     };
 
     for triangle in triangles {
-        let triangle = &vec![(triangle.0.x as i32, triangle.0.y as i32), (triangle.1.x as i32, triangle.1.y as i32), (triangle.2.x as i32, triangle.2.y as i32)]; // TODO: Simplify this by implementing Into<Vec<(T,T)>> for Triangle in cazan common
+        let triangle = &vec![
+            (triangle.0.x as i32, triangle.0.y as i32),
+            (triangle.1.x as i32, triangle.1.y as i32),
+            (triangle.2.x as i32, triangle.2.y as i32),
+        ]; // TODO: Simplify this by implementing Into<Vec<(T,T)>> for Triangle in cazan common
 
-        root.draw(&Polygon::new(
-            triangle.clone(),
-            RED.mix(0.3))
-        )?;
-        chart.draw_series(LineSeries::new(
-            triangle.iter().chain(std::iter::once(&triangle[0])).map(|&(x, y)| (x as u32, y as u32)),
-            line_style,
-        )).unwrap();
+        root.draw(&Polygon::new(triangle.clone(), RED.mix(0.3)))?;
+        chart
+            .draw_series(LineSeries::new(
+                triangle
+                    .iter()
+                    .chain(std::iter::once(&triangle[0]))
+                    .map(|&(x, y)| (x as u32, y as u32)),
+                line_style,
+            ))
+            .unwrap();
     }
 
     root.present()?;
